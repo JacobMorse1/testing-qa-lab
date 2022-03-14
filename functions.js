78a0c9bd111f897f1dcf12346dfb1a9f8d1133a1
Jacob Morse <jacobsmorse@gmail.com>
2022-03-14 16:07:06 -0600

    finished lab

--- functions.js
+++ functions.js
@@ -7,8 +7,21 @@ module.exports = {
     greeting: (name) => {
         return `Hello, ${name}.`
     },
+    
     add: (num1, num2) => {
         return num1 + num2
+    },
+
+    subtract: (num1, num2) => {
+        return num1 - num2
+    },
+
+    multiply: (num1, num2) => {
+        return num1 * num2
+    },
+
+    divide: (num1, num2) => {
+        return num1 / num2
     }
 
 }
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,5 @@
 const { TestWatcher } = require('jest')
-const {returnTwo, greeting, add} = require('./functions.js')
+const {returnTwo, greeting, add, subtract, multiply, divide} = require('./functions.js')
 
 
 test('returnTwo should return 2', () => {
@@ -10,8 +10,25 @@ test('greeting should equal hello, (name)', () => {
     expect(greeting('James')).toEqual("Hello, James.")
     expect(greeting('Jill')).toEqual("Hello, Jill.")
 });
+describe('Math Functions', () => {
 
-test('sum of num1 and num2', () => {
-    expect(add(1, 2)).toEqual(3)
-    expect(add(5, 9)).toEqual(14)
+    test('sum of num1 plus num2', () => {
+        expect(add(1, 2)).toEqual(3)
+        expect(add(5, 9)).toEqual(14)
+    });
+    
+    test('sum of num1 subtract num2', () => {
+        expect(subtract(16, 13)).toEqual(3)
+        expect(subtract(7, 10)).toEqual(-3)
+    });
+    
+    test('sum of num1 multiplied by num2', () => {
+        expect(multiply(10, 15)).toEqual(150)
+        expect(multiply(40, .25)).toEqual(10)
+    });
+    
+    test('sum of num1 divided by num2', () => {
+        expect(divide(21, 3)).toEqual(7)
+        expect(divide(360, 90)).toEqual(4)
+    });
 });
